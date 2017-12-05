c481d6e4548b3716d77691dd878bf32d5b80a20d
Miguel <miguelmoutela@hotmail.com>
2017-12-05 20:05:55 +0000

    minor typos sorted

--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
-const Dinossaur = require('../dinosaur.js');
+const Dinosaur = require('../dinosaur.js');
 
 describe('Dinosaur', function(){
   let dinosaur;
   beforeEach(function(){
     dinosaur = new Dinosaur('Tyrannosaurus', 3);
-  })
+  });
 
   it('should have a type', function(){
     assert.strictEqual(dinosaur.type, 'Tyrannosaurus');
-  })
-})
+  });
+});
